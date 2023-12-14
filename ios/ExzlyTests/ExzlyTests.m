7044e1a261f2986529759c94805280cb15296740
Agung Dirgantara <agungmasda29@gmail.com>
2023-12-14 15:06:41 +0700

    chore(rename): start development :fire:

--- ios/ExzlyTests/ExzlyTests.m
+++ ios/ExzlyTests/ExzlyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ExzlyRNBareTests : XCTestCase
+@interface ExzlyTests : XCTestCase
 
 @end
 
-@implementation ExzlyRNBareTests
+@implementation ExzlyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
